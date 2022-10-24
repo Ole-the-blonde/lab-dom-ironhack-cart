3e9f714768f68c014162d74dc17f7f80d719fe9b
Ole Demay <olesyademy@gmail.com>
2022-10-24 09:48:04 +0200

    Half done

--- js/index.js
+++ js/index.js
@@ -1,21 +1,31 @@
 // ITERATION 1
 
+const { product } = require('puppeteer');
+
 function updateSubtotal(product) {
   console.log('Calculating subtotal, yey!');
-
-  //... your code goes here
+  const price = product.querySelector('.price span');
+  const quantity = product.querySelector('.quantity input');
+  console.log(price.textContent, quantity.value);
+  const subtotal = product.querySelector('.subtotal span');
+  console.log(subtotal.textContent);
+  subtotal.textContent = price.textContent * quantity.value;
+  return subtotal;
 }
 
 function calculateAll() {
   // code in the following two lines is added just for testing purposes.
   // it runs when only iteration 1 is completed. at later point, it can be removed.
-  const singleProduct = document.querySelector('.product');
-  updateSubtotal(singleProduct);
+  // const singleProduct = document.querySelector('.product');
+  // updateSubtotal(singleProduct);
   // end of test
 
-  // ITERATION 2
-  //... your code goes here
-
+  let sum = 0;
+  let allProducts = document.getElementsByClassName('product');
+  for (let i = 0; i < allProducts.length; i++) {
+    updateSubtotal(allProducts[i]);
+    sum += updateSubtotal(allProducts[i]);
+  }
   // ITERATION 3
   //... your code goes here
 }
